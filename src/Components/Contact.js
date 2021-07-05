78b53086154c8771ed90aa0625904060ea18bb88
orsel <orselv99@gmail.com>
2021-07-05 23:23:02 +0900

    contact 데이터 추가

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,6 +1,53 @@
 import React from 'react';
 import ContactInfo from './ContactInfo';
 import ContactDetail from './ContactDetail';
+import ContactCreate from './ContactCreate';
+
+/*
+1.
+state 의 배열을 기본 js 메서드로 push 하면 배열자체를 변경하기때문에 사용하면 안됨
+대신, concat 을 사용 (기존 배열은 그대로 두고 새 배열을 만듬)
+this.setState({
+    list: this.state.list.concat(newObj)
+});
+
+2.
+immutability helper (immutable.js) npm install react-addons-update --save
+# 원소 추가
+this.setState({
+    list: update(
+        this.state.list, 
+        { 
+            $push: [newObj, newObj2, ...]   // 하나여도 배열형태로
+        }
+    );
+});
+
+# 원소 제거
+this.setState({
+    list: update(
+        this.state.list, 
+        { 
+            $splice: [[index, 1], ...]  // 주의! 처음 명령으로 array 에서 원소를 지우면 index 가 변경됨
+        }
+    );
+});
+
+# 원소 제거
+this.setState({
+    list: update(
+        this.state.list, 
+        { 
+            [index] : {
+                field1: { $set: 'value1' },
+                field2: { $set: 'value2' },
+            }
+        }
+    );
+});
+
+*/
+import update from 'react-addons-update';
 
 export default class Contact extends React.Component {
     constructor(props) {
@@ -17,8 +64,13 @@ export default class Contact extends React.Component {
         }
         this.handleChange = this.handleChange.bind(this);
         this.handleClick = this.handleClick.bind(this);
+
+        this.handleCreate = this.handleCreate.bind(this);
+        this.handleRemove = this.handleRemove.bind(this);
+        this.handleEdit = this.handleEdit.bind(this);
     }
 
+    // 사용자 이벤트
     handleChange(e) {
         this.setState({
             keyword: e.target.value
@@ -29,6 +81,37 @@ export default class Contact extends React.Component {
             selectedKey: key
         });
         console.log(`'${key}' is selected!`);
+    }
+    handleCreate(contact) {
+        this.setState({
+            contactData: update(
+                this.state.contactData,
+                {
+                    $push: [contact],
+                }
+            ),
+        });
+    }
+    handleRemove() {
+        this.setState({
+            contactData: update(
+                this.state.contactData,
+                {
+                    // '현재 선택된 키' 부터 하나의 원소제거
+                    $splice: [[this.state.selectedKey, 1]],
+                }
+            ),
+        });
+    }
+    handleEdit(name, number) {
+        this.setState({
+            contactData: update({
+                [this.state.selectedKey]: {
+                    name: { $set: name },
+                    number: { $set: number },
+                },
+            }),
+        });
     }
 
     render() {
@@ -54,6 +137,9 @@ export default class Contact extends React.Component {
                 <ContactDetail 
                     isSelected={this.state.selectedKey != -1 ? true : false}
                     contactData={this.state.contactData[this.state.selectedKey]}
+                />
+                <ContactCreate 
+                    onClick={this.handleCreate}
                 />
             </div>
         );
---
+++ src/Components/ContactCreate.js
@@ -0,0 +1,68 @@
+import React from 'react';
+
+export default class ContactCreate extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            number: '',
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleChange(e) {
+        // 여러 이벤트를 하나의 메서드에서 같이 처리하기 위해 아래와 같이 빈 객체를 생성
+        let nextState = {};
+        nextState[e.target.name] = e.target.value;
+        this.setState(nextState);
+    }
+    handleClick() {
+        const contact = {
+            name: this.state.name,
+            number: this.state.number,
+        }
+        this.props.onClick(contact);
+
+        // 컨트롤의 value 초기화
+        this.setState({
+            name: '',
+            number: '',
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>Create Contact</h2>
+                <p>
+                <input 
+                    type='text'
+                    name='name'
+                    value={this.state.name}
+                    placeholder='name'
+                    onChange={this.handleChange}
+                />
+                <input 
+                    type='text'
+                    name='number'
+                    value={this.state.number}
+                    placeholder='number'
+                    onChange={this.handleChange}
+                />
+                </p>
+                <button onClick={this.handleClick}>Create</button>
+            </div>
+        );
+    }
+}
+
+// ContactCreate.propTypes = {
+//     onCreate: React.PropTypes.func
+// }
+// ContactCreate.defaultProps = {
+//     onCreate: () => {
+//         console.log('Does not create onCreate. ');
+//     }
+// }
