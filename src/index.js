206d5c05dd4a488d684d8d9de9a12bfcaff04624
orseL <orseLv99@gmail.com>
2021-07-05 10:20:38 +0900

    init

--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class Counter extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: 0
+        }
+        this.onClick = this.onClick.bind(this);
+    }
+
+    onClick() {
+        this.setState({
+            value: this.state.value + 1,
+        });
+    }
+    render() {
+        return (
+            <div>
+                <h2>{this.state.value}</h2>
+                <button onClick={this.onClick}>press me!</button>
+            </div>
+        );
+    }
+}
+
+class App extends React.Component {
+    render() {
+        return (
+            <Counter />
+        );
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
